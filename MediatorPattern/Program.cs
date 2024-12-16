add74267bfded789f56f995ab0d9c757cf64070c
DxMort <99000786+DxMort@users.noreply.github.com>
2024-12-16 11:49:15 +0700

    Add files via upload

---
+++ MediatorPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using Mediator_Pattern_Chat;
+
+
+namespace ChatSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatMediator chatMediator = new ChatMediator();
+
+            User user1 = new User("Денис", chatMediator);
+            User user2 = new User("Егойчик", chatMediator);
+            User user3 = new User("Абоб", chatMediator);
+
+            chatMediator.AddUser(user1);
+            chatMediator.AddUser(user2);
+            chatMediator.AddUser(user3);
+
+            user1.SendMessage("Бобр курва!", "Егойчик");
+            user2.SendMessage("Я пердоле!", "Денис");
+            user3.SendMessage("Когда спать?", "Егойчик");
+
+            Console.WriteLine();
+            chatMediator.ShowMessageHistory("Денис");
+            Console.WriteLine();
+            chatMediator.ShowMessageHistory("Егойчик");
+            Console.WriteLine();
+            chatMediator.ShowMessageHistory("Абоба");
+
+            Console.WriteLine();
+            chatMediator.RemoveUser("Абоба");
+        }
+    }
+}
